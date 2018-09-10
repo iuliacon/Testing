a0e57432e770cb410b7464251886e1603e3bbc26
iuliacon <conut.iulia@gmail.com>
2018-09-10 14:45:35 +0300

    Added first test

--- FirstApp/FirstApp/Guru99.cs
+++ FirstApp/FirstApp/Guru99.cs
@@ -3,25 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools;
-using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
-
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace FirstApp
 {
-    class Program
+    [TestClass]
+    public class Guru99
     {
-        static void Main(string[] args)
+        private IWebDriver browser;
+
+        [TestInitialize]
+        public void startBrowser()
         {
-            IWebDriver browser = new ChromeDriver();
+            browser = new ChromeDriver();
             browser.Url = "https://www.guru99.com/";
             browser.Manage().Window.Maximize();
             browser.Navigate().GoToUrl("https://www.guru99.com/");
             browser.Manage().Timeouts().ImplicitWait = new TimeSpan(0, 0, 5);
             browser.Manage().Timeouts().AsynchronousJavaScript = new TimeSpan(0, 0, 10);
             browser.Manage().Timeouts().PageLoad = new TimeSpan(0, 0, 15);
+        }
 
+        [TestCleanup]
+        public void TearDown()
+        {
+            browser.Close();
+        }
+
+
+        [TestMethod]
+        public void Test1()
+        {
+            Console.WriteLine("Start test"); ;
+
+            Thread.Sleep(10000);
+
+            Console.WriteLine("Stop test"); ;
+
+           
         }
     }
 }
